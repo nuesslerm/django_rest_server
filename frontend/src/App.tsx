952ed66d826d979d374e2ef7936a9cc80ffcd624
nuesslerm <markus@choco.com>
2021-01-31 04:57:40 +0100

    react integration using webpack and babel

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import Main from './components/Main';
+
+const App: FC = () => {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/webpack.config.ts
@@ -0,0 +1,36 @@
+import path from 'path';
+import webpack from 'webpack';
+
+module.exports = {
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devtool: 'inline-source-map',
+  entry: '.src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, '.static/js'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        // This has effect on react lib size
+        NODE_ENV: '"production"',
+      },
+    }),
+  ],
+};
